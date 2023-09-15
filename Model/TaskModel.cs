727f0720d5feb7bcf6011dcb1af052c194ca1862
Marcuszz <102037920+Marcuszz@users.noreply.github.com>
2023-09-15 11:25:50 +0800

    Add files via upload

---
+++ Model/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace App1.Model
+{
+     class TaskModel
+    {
+        public string Subjectcode { get; set; }
+
+        public string SubjectTitle { get; set; }
+
+        public int Unit { get; set; }
+
+    }
+}
